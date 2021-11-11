082c54746afa96918ac3f3a126c0e60abe2451fa
vita shela <vitashela@vitade-MBP.Dlink>
2021-11-12 01:12:44 +0800

    add react-paginate pacjage and add basic paginatedItems component

---
+++ src/components/PaginatedItems/index.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import ReactPaginate from 'react-paginate'
+import PropTypes from 'prop-types'
+
+const PaginatedItems = ({ itemsPerPage, items, setCurrentItems }) => {
+  const [pageCount, setPageCount] = useState(0)
+  const [itemOffset, setItemOffset] = useState(0)
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage
+    // console.log(`Loading items from ${itemOffset} to ${endOffset}`)
+    setCurrentItems(items.slice(itemOffset, endOffset))
+    setPageCount(Math.ceil(items.length / itemsPerPage))
+  }, [itemOffset, items, itemsPerPage, setCurrentItems])
+
+  const handlePageClick = useCallback(
+    (event) => {
+      const newOffset = (event.selected * itemsPerPage) % items.length
+      // console.log(
+      //   `User requested page number ${event.selected}, which is offset ${newOffset}`,
+      // )
+      setItemOffset(newOffset)
+    },
+    [items.length, itemsPerPage],
+  )
+
+  return (
+    <ReactPaginate
+      nextLabel='next >'
+      onPageChange={handlePageClick}
+      pageRangeDisplayed={5}
+      pageCount={pageCount}
+      previousLabel='< previous'
+      pageClassName='page-item'
+      pageLinkClassName='page-link'
+      previousClassName='page-item'
+      previousLinkClassName='page-link'
+      nextClassName='page-item'
+      nextLinkClassName='page-link'
+      breakLabel='...'
+      breakClassName='page-item'
+      breakLinkClassName='page-link'
+      containerClassName='pagination'
+      activeClassName='active'
+      renderOnZeroPageCount={null}
+    />
+  )
+}
+
+PaginatedItems.propTypes = {
+  itemsPerPage: PropTypes.number.isRequired,
+  items: PropTypes.array.isRequired,
+  setCurrentItems: PropTypes.func.isRequired,
+}
+
+export default PaginatedItems
