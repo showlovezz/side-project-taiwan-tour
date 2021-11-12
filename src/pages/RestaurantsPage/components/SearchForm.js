3d2a157ff49032d05b92325e1a80f2f5862ed795
vita shela <vitashela@vitade-MacBook-Pro.local>
2021-11-12 10:12:07 +0800

    add restaurants page

---
+++ src/pages/RestaurantsPage/components/SearchForm.js
@@ -0,0 +1,91 @@
+import React, { useCallback } from 'react'
+import { Button, Form, Row, Col } from 'react-bootstrap'
+import { Formik, Form as FormikForm } from 'formik'
+
+import InputField from '../../../components/SearchForm/components/InputField'
+import SelectField from '../../../components/SearchForm/components/SelectField'
+import { cityOptions } from '../../../components/SearchForm/configs/optionConfigs'
+import { useDataContext } from '../../../components/hooks/DataProvider'
+
+import validationSchema from '../validations'
+import {
+  fetchAllRestaurants,
+  fetchCityRestaurants,
+  fetchKeyWordWithCityRestaurants,
+  fetchKeyWordRestaurants,
+} from '../api'
+
+const SearchForm = () => {
+  const { setLoading, setRestaurants } = useDataContext()
+  const searchRule = (keyWord, city) => {
+    if (keyWord && city) return fetchKeyWordWithCityRestaurants(keyWord, city)
+    if (city) return fetchCityRestaurants(city)
+    if (keyWord) return fetchKeyWordRestaurants(keyWord)
+
+    return fetchAllRestaurants()
+  }
+
+  const handleSubmit = useCallback(
+    (values, { setSubmitting }) => {
+      const { key_word: keyWord, city } = values
+      setLoading(true)
+      return searchRule(keyWord, city)
+        .then((restaurants) => {
+          setSubmitting(false)
+          setRestaurants(restaurants)
+          setLoading(false)
+        })
+        .catch((error) => {
+          setSubmitting(false)
+          console.log(error)
+        })
+    },
+    [setLoading, setRestaurants],
+  )
+
+  return (
+    <section className='tours-page__search'>
+      <Formik
+        initialValues={{ key_word: '', city: '' }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <FormikForm>
+          <Row className='mb-3'>
+            <Form.Group as={Col}>
+              <InputField
+                name='key_word'
+                className='tours-page__search-input'
+              />
+            </Form.Group>
+          </Row>
+          <Row className='mb-3'>
+            <Form.Group as={Col}>
+              <SelectField name='city' className='tours-page__search-select'>
+                {cityOptions.map(({ label, value }, index) => {
+                  return (
+                    <option key={index} value={value}>
+                      {label}
+                    </option>
+                  )
+                })}
+              </SelectField>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Col className='d-flex justify-content-end'>
+              <Button type='reset' variant='outline-secondary' className='me-3'>
+                清除
+              </Button>
+              <Button type='submit' variant='outline-primary'>
+                搜尋
+              </Button>
+            </Col>
+          </Row>
+        </FormikForm>
+      </Formik>
+    </section>
+  )
+}
+
+export default SearchForm
--- src/pages/RestaurantsPage/index.js
+++ src/pages/RestaurantsPage/index.js
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Container, Row, Col, Breadcrumb } from 'react-bootstrap'
+
+import Loading from '../../components/Loading'
+import PaginatedItems from '../../components/PaginatedItems'
+import { useDataContext } from '../../components/hooks/DataProvider'
+
+import SearchForm from './components/SearchForm'
+import ProductCardGroup from './components/ProductCardGroup'
+import { fetchAllRestaurants } from './api'
+import './styles.scss'
 
 const RestaurantsPage = () => {
+  const {
+    loading,
+    setLoading,
+    restaurants,
+    setRestaurants,
+    currentItems,
+    setCurrentItems,
+  } = useDataContext()
+
+  useEffect(() => {
+    setLoading(true)
+    fetchAllRestaurants().then((restaurants) => {
+      setRestaurants(restaurants)
+      setLoading(false)
+    })
+  }, [setRestaurants, setLoading])
+
   return (
     <section className='restaurants-page'>
-      <h2 className='text-center'>推薦餐廳</h2>
-      <div>RestaurantsPage</div>
+      <hr className='mt-0' />
+      <Container>
+        <Row>
+          <Col>
+            <Breadcrumb className='restaurants-page__breadcrumb'>
+              <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
+              <Breadcrumb.Item active>More Restaurants</Breadcrumb.Item>
+            </Breadcrumb>
+          </Col>
+        </Row>
+        <hr className='mt-0' />
+        <Row>
+          <Col md={3}>
+            <SearchForm />
+          </Col>
+          {loading ? (
+            <Loading />
+          ) : (
+            <Col md={9}>
+              <h3 className='tours-page__result'>{`共有 ${restaurants.length} 筆搜尋結果`}</h3>
+              <Row>
+                <ProductCardGroup data={currentItems} />
+              </Row>
+              <Row>
+                <Col className='d-flex justify-content-center'>
+                  <PaginatedItems
+                    itemsPerPage={9}
+                    items={restaurants}
+                    setCurrentItems={setCurrentItems}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          )}
+        </Row>
+      </Container>
     </section>
   )
 }
